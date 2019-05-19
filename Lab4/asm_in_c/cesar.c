194ff39b679c137d9671c40cc722552e2c930d39
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-05-19 20:35:36 +0200

    Made C with asm program work properly

--- Lab4/asm_in_c/cesar.c
+++ Lab4/asm_in_c/cesar.c
@@ -1,45 +1,64 @@
 #include <stdio.h>
  
 // Deklaracja zmiennej przechowującej ciąg znaków do konwersji
-char str[] = "AbCdEfGh";
+char str[] = "ALAmaKOTAnieMApsa";
 // Stała przechowująca długość tego ciągu
-const int len = 8;
-const int key = 1;
+int len = 8;
+int key = 30;
  
 int main(void)
 {
-    //
-    // Wstawka Asemblerowa
-    //
     asm(
-    "mov $0, %%rbx \n" // licznik petli 
+    // W rejestrze RDI znajdzie się wskaźnik na pierwszą komórkę stringa.
+	// W rejestrze RDX znajdzie sie klucz do szyfru
+    "mov %0, %%rdi \n"
+    "mov key, %%rdx \n"
 
-    "loop: \n" // Etykieta powrotu do pętli
- 
-    "mov (%0, %%rbx, 1), %%al \n" // Skopiowanie n-tej komórki stringa
-    // do rejestru Al. %0 to alias rejestru w którym kompilator C umieści
-    // pierwszy parametr wejściowy (wskaźnik na pierwszą komórkę stringa).
- 
-    "cmp %%al, 'Z' \n"    //Ominiecie wszystkich wielkich liter
-    "jle save \n" 
-    "add key, %%al \n"
+    // // indeks
+	"mov %%rdx, %%rax \n"
+	"mov $0, %%rdx \n"
+	
+	// // liczenie modulo klucza, wynik nadal w RDX
+	"mov $26, %%r11 \n"
+	"idiv %%r11 \n" 
+
+    "mov $0, %%rax \n"
 
 
-    "save: \n"
-    "mov %%al, (%0, %%rbx, 1) \n" // Zapisanie zmienionej wartości do stringa
+	"loop: \n"
+		"mov (%%rdi, %%rax, 1), %%bl \n"
+		"cmp $'z', %%bl \n"
+		"jg skip_char \n"
+		"cmp $'a', %%bl \n"
+		"jl skip_char \n"
+
+		// dodanie klucza z rdx do znaku w rbx
+		"add %%dl, %%bl \n" 
+		// nowy znak juz gotowy
+
+		// sprawdzenie czy nie wyszlismy poza zakres malych liter
+		"cmp $122, %%bl \n"
+		"jle skip_char \n"
+		"sub $26, %%rbx \n"
+
+		"skip_char: \n"
+    	"mov %%bl, (%%rdi, %%rax, 1) \n"
+
+	"inc %%rax \n"
+
+	// rozwiazanie bez licznika :)
+	"cmp $999, %%rax \n"
+	"jne loop \n"
+
+	:
+
+	// Lista parametrów wejściowych - są one dostępne jako aliasy na rejestry - %0, %1, %2
+    :"r"(&str)
  
-    "inc %%rbx \n"      // Zwiększenie licznika pętli
-    "cmp len, %%rbx \n" // Porównanie licznika pętli ze stałą "len"
-    "jl loop \n" // Powrót na początek pętli aż do wykonania operacji
- 
-    : // Brak parametow wyjsciowych
- 
-    :"r"(&str) // Lista parametrów wejściowych
- 
-    :"%rax", "%rbx" // Rejestry których będziemy używać w kodzie Asemblerowym.
+	// Rejestry których będziemy używać w kodzie Asemblerowym.
+    :"%rax", "%rbx", "%rdi", "%rsi", "%rdx", "%r11" 
     );
  
-    // Wyświetlenie wyniku
     printf("Wynik: %s\n", str);
     return 0;
 }
